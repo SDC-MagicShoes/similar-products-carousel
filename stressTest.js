89316448f8625e49a9edb98a8ec216b2a258b795
edenlhn <edenlhn@gmail.com>
2018-11-27 11:32:08 -0800

    Minor change to stressTest file

--- stressTest.js
+++ stressTest.js
@@ -1,5 +1,5 @@
 import http from 'k6/http';
-import { check, sleep } from 'k6';
+import { sleep } from 'k6';
 
 export let options = {
   vus: 200,
@@ -8,10 +8,6 @@ export let options = {
 }
 
 export default function() {
-  let res = http.get('http://localhost:8081/9310200/similar');
-  check(res, {
-    "status was 200": (r) => r.status == 200,
-    "transaction time OK": (r) => r.timings.duration < 200
-  });
+  http.get('http://localhost:8081/9310200/similar');
   sleep(1);
 }
